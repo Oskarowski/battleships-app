544fac5157e2a0af98cf9c6a28b5f331e9dcc3c2
Oskarowski <oscar.kacprzak@gmail.com>
2023-03-05 21:35:40 +0100

    init of after game scene

---
+++ src/components/EndGameComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    :class="{
+      'end-game': true,
+    }"
+  ></div>
+  <h1>END OF THE GAME</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.end-game {
+  background-color: goldenrod;
+}
+</style>
