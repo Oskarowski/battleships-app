f6fbf5c6fd0f8b6107534ae51c326bc413145a21
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2023-03-09 23:17:02 +0100

    added github logo btn and working credits

--- src/components/AboutAuthor.vue
+++ src/components/AboutAuthor.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container">
-    <h1>About Author</h1>
-    <h3>by: Oskar Kacprzak</h3>
-    <a href="https://github.com/Oskarowski" target="_blank" rel="noopener"
-      >GitHub</a
-    >
+    <h1>by: Oskar Kacprzak</h1>
+    <h2 onclick="window.open('https://fundacja.addictive.pl/')">
+      chaired by: Fundacja Grupy Addictive
+    </h2>
+    <button
+      class="git-btn"
+      onclick="window.open('https://github.com/Oskarowski')"
+    ></button>
+
     <button @click="goBackBtnClicked" class="btn">Go Back</button>
   </div>
 </template>
@@ -26,7 +30,6 @@ export default {
   width: 100%;
   height: 100%;
   backdrop-filter: blur(30px);
-  background-color: azure;
   text-align: center;
   display: flex;
   flex-direction: column;
@@ -35,7 +38,7 @@ export default {
 }
 .btn {
   padding: 2rem;
-  background-color: green;
+  background-color: rgb(115, 147, 179);
   font-size: 2em;
   border-radius: 1em;
   border: 0;
@@ -43,6 +46,30 @@ export default {
   color: white;
 }
 .btn:hover {
+  opacity: 0.9;
+  cursor: pointer;
+}
+
+h2:hover {
+  opacity: 0.9;
+  cursor: pointer;
+  font-size: 1.6em;
+}
+
+.git-btn {
+  background-image: url("../assets/GithubLogo.png");
+  background-size: cover;
+  background-position: center;
+  color: transparent;
+  cursor: pointer;
+  padding: 0;
+  margin: 1rem;
+  width: 18em;
+  height: 18em;
+  border-radius: 50%;
+  border: 0px solid rgb(115, 147, 179);
+}
+.git-btn:hover {
   opacity: 0.9;
   cursor: pointer;
 }
