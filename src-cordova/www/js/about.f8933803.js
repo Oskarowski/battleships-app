206c8ddf53e151d57d3e1611b531b96d9d6d0510
Oskarowski <oscar.kacprzak@gmail.com>
2023-04-17 14:25:01 +0200

    del src-cordova

--- src-cordova/www/js/about.f8933803.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkbattleships_app"]=self["webpackChunkbattleships_app"]||[]).push([[443],{7381:function(t,a,n){n.r(a),n.d(a,{default:function(){return h}});var s=n(3396);const e={class:"about"},u=(0,s._)("h1",null,"This is an about page",-1),r=[u];function i(t,a){return(0,s.wg)(),(0,s.iD)("div",e,r)}var p=n(89);const c={},l=(0,p.Z)(c,[["render",i]]);var h=l}}]);
-//# sourceMappingURL=about.f8933803.js.map
